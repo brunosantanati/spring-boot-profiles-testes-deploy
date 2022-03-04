556e442dbed29299cd2684fbbca3d6b406498e61
Bruno Rafael Sant Ana <bruno.santana.ti@gmail.com>
2022-03-04 16:47:10 -0300

    Revert "Gerar war no lugar de jar."
    This reverts commit 11713fe6cf5f1a02b3d8aea3d8ec2af5e2494aa2.

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,8 +2,6 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
@@ -13,17 +11,12 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSpringDataWebSupport
 @EnableCaching
 @EnableSwagger2
-public class ForumApplication extends SpringBootServletInitializer {
+public class ForumApplication {
 
 	//Configure this VM argument before run this class in dev environment: -Dspring.profiles.active=dev
 	//prod configuration: -Dspring.profiles.active=prod
 	public static void main(String[] args) {
 		SpringApplication.run(ForumApplication.class, args);
-	}
-	
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(ForumApplication.class);
 	}
 
 }
